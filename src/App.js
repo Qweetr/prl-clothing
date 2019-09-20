79033720dfa979641f3d8280c64c3148dfd18271
FerrBen <qweetr86@gmail.com>
2019-09-20 10:35:25 +0200

    adding shop page components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -39,6 +40,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
         <Route exact path='/shop/hats' component={HatsPage} />
         <Route exact path='/shop/jackets' component={JacketsPage} />
         <Route exact path='/shop/shoes' component={ShoesPage} />
