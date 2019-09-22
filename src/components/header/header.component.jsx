5ba10cbda93cc576fc8dd64bbacb459be53408e7
FerrBen <qweetr86@gmail.com>
2019-09-22 13:50:13 +0200

    addind sign in, header and custom button components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import logo from '../../assets/goku.png';
 
 import './header.styles.scss';
 
 const Header = () => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+      <img className="logo" src={logo} alt="Logo" />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
