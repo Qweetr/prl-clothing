e3c734e79c43433738b09e36928739b107cba88c
FerrBen <qweetr86@gmail.com>
2019-09-23 22:56:24 +0200

    changed some stuff in header, sign-in and custom-button component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 
 import logo from '../../assets/goku.png';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <img className="logo" src={logo} alt="Logo" />
+      <img className='logo' src={logo} alt='Logo' />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
@@ -17,6 +18,15 @@ const Header = () => (
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
