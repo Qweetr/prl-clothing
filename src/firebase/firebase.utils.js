8c8ac69738d9b7835e8baaf7e1773ca86beaaddf
FerrBen <qweetr86@gmail.com>
2019-09-24 12:28:55 +0200

    storing user data in App

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,23 +3,46 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyClFQBTEYDYntbBn4JxF5JmeXZYFv2Yle8",
-  authDomain: "prl-clothing.firebaseapp.com",
-  databaseURL: "https://prl-clothing.firebaseio.com",
-  projectId: "prl-clothing",
-  storageBucket: "",
-  messagingSenderId: "909485946799",
-  appId: "1:909485946799:web:84871d1eb87add4632615a"
+  apiKey: 'AIzaSyClFQBTEYDYntbBn4JxF5JmeXZYFv2Yle8',
+  authDomain: 'prl-clothing.firebaseapp.com',
+  databaseURL: 'https://prl-clothing.firebaseio.com',
+  projectId: 'prl-clothing',
+  storageBucket: '',
+  messagingSenderId: '909485946799',
+  appId: '1:909485946799:web:84871d1eb87add4632615a'
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
-
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
